868fef69077589b7c2c5ad491775acb150f3a0f6
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-04-12 14:04:46 +0530

    Add Solution

--- Leetcode-30-Days-js/Solution-02.js
+++ Leetcode-30-Days-js/Solution-02.js
@@ -4,15 +4,15 @@
  */
 var createCounter = function(n) {
     
-  return function() {
-     return n++;
-      
-  };
+    return function() {
+       return n++;
+        
+    };
 };
 
 /** 
-* const counter = createCounter(10)
-* counter() // 10
-* counter() // 11
-* counter() // 12
-*/
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
