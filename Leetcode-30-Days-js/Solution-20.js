33c1c385151f3663df2c7747e419fb137b8732b9
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-03 11:55:01 +0530

    Is Object Empty

---
+++ Leetcode-30-Days-js/Solution-20.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+  if(JSON.stringify(obj).length <= 2){
+      return true;
+  } else {
+      return false;
+  }
+};
